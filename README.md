f105d616f730a3b84cc2af37915d9579520b714d
Yusuke Sugomori <me@yusugomori.com>
2016-12-07 03:26:08 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RNN.js
+
+Simple Recurrent Neural Networks (RNN) implementation with JavaScript.
+
+
+```
+$ npm install
+$ node main.js
+```
---
+++ math/operator/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  add:       require('./add'),
+  argmax:    require('./argmax'),
+  dimension: require('./dimension'),
+  divide:    require('./divide'),
+  dot:       require('./dot'),
+  dim:       require('./dimension'),
+  max:       require('./max'),
+  min:       require('./min'),
+  multiply:  require('./multiply'),
+  mul:       require('./multiply'),
+  outer:     require('./outer'),
+  subtract:  require('./subtract'),
+  sub:       require('./subtract'),
+  sum:       require('./sum'),
+  transpose: require('./transpose'),
+  T:         require('./transpose')
+}
